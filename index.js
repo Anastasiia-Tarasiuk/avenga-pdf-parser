00837049df595f765310bf6f8f436f722ecae6d5
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2023-01-05 17:35:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
+console.log(buttonEl)
+
+buttonEl.addEventListener("click", saveFile);
+
+async function saveFile(filePath) {
+
+}
+
